aeca2cab1630108e9f7faf92788cc88aa3843703
Eljo George <egeorge1@live.maryville.edu>
2021-04-10 15:52:22 -0700

    Social sharing and android platform support

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,6 +3,7 @@ import { ToastController } from '@ionic/angular';
 import { Item } from '../models/models';
 import { GroceriesService } from '../services/groceries.service';
 import { InputDialogService } from '../services/input-dialog.service';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @Component({
   selector: 'app-home',
@@ -13,7 +14,7 @@ export class HomePage {
 
   title = "Grocery"
 
-  constructor(private toastController: ToastController, private groceryService: GroceriesService, private inputDialogService: InputDialogService) { }
+  constructor(private toastController: ToastController, private groceryService: GroceriesService, private inputDialogService: InputDialogService, private socialSharing: SocialSharing) { }
 
   public loadItems() {
     return this.groceryService.getItems();
@@ -54,5 +55,19 @@ export class HomePage {
     await toast.present();
 
     await this.inputDialogService.showPrompt(item, index);
+  }
+
+  /**
+ * API to share an item.
+ */
+  public async shareItem(item: Item) {
+    console.log("Sharing", item);
+    const message = `Grocery Item- Name: ${item.name}, Qty: ${item.quantity}`;
+    try {
+      await this.socialSharing.share(message)
+      console.log('Successfully shared', item);
+    } catch (e) {
+      console.error('Sharing failed', e);
+    }
   }
 }
